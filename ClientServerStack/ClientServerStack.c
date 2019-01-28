4383ee13d3436e6e4c8630ad3f0a213b8e006ec1
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-01-28 21:37:55 +0300

    Bag of dicks

--- ClientServerStack/ClientServerStack.c
+++ ClientServerStack/ClientServerStack.c
@@ -1,6 +1,18 @@
 #include <Windows.h>
 #include <stdio.h>
 
+const char *PIPENAME = L"\\\\.\\pipe\\StackPipe351";
+
+#define STACKPICK 0
+#define STACKPOP 1
+#define STACKPUSH 2
+#define STACKNEW 3
+#define STACKSIZE 4
+#define STACKISEMPTY 5
+#define STACKDISPLAY 6
+
+#pragma region Stack
+
 typedef struct StackNode {
 	struct StackNode *prev;
 	void *data;
@@ -26,7 +38,7 @@ char stackPush(Stack *stack, void *data) {
 		return -1;
 	}
 
-	StackNode *newNode = (StackNode*) malloc(sizeof(StackNode));
+	StackNode *newNode = (StackNode*) HeapAlloc(GetProcessHeap(), 0, sizeof(StackNode));
 	if (!newNode) {
 		return -1;
 	}
@@ -53,8 +65,33 @@ void *stackPick(Stack *stack) {
 	return stack->top->data;
 }
 
+void *stackPop(Stack *stack) {
+	if (stack->top == NULL) {
+		return NULL;
+	}
+
+	void *picked = stackPick(stack);
+
+	StackNode *old = stack->top;
+	stack->top = stack->top->prev;
+	HeapFree(GetProcessHeap(), 0, old);
+	stack->size--;
+
+	return picked;
+}
+
+void stackClear(Stack *stack) {
+	while (stack->size > 0) {
+		stackPop(stack);
+	}
+}
+
 char stackPushInt(Stack *stack, int val) {
-	int *data = malloc(sizeof(int));
+	int *data = (int*) HeapAlloc(GetProcessHeap(), 0, sizeof(int));
+	if (!data) {
+		return -1;
+	}
+
 	*data = val;
 
 	return stackPush(stack, data);
@@ -62,14 +99,115 @@ char stackPushInt(Stack *stack, int val) {
 
 int stackPickInt(Stack *stack) {
 	int *picked = (int*) stackPick(stack);
+	if (!picked) {
+		return -1;
+	}
+
 	return *picked;
 }
 
-int main() {
-	Stack stack = emptyStack();
-	stackPushInt(&stack, 124);
+int stackPopint(Stack *stack) {
+	int *picked = (int*) stackPop(stack);
+	if (!picked) {
+		return -1;
+	}
 
-	printf("%d", stackPickInt(&stack));
+	return *picked;
+}
+
+#pragma endregion
+
+void displaySatckOfInts(Stack *stack) {
+	StackNode *current = stack->top;
+	printf("\nSTACK TOP\n____________\n");
+	while (current) {
+		printf("%d\n", *(int*) current->data);
+		current = current->prev;
+	}
+	printf("____________\nSTACK BOTTOM\n\n");
+}
+
+void serverJob(LPVOID lpParam) {
+	HANDLE hPipe = CreateNamedPipe(
+		PIPENAME,
+		PIPE_ACCESS_DUPLEX,
+		PIPE_READMODE_BYTE,
+		1, 0, 0, 0, NULL
+	);
+
+	if (hPipe == INVALID_HANDLE_VALUE || hPipe == NULL) {
+		printf("Server: Failed to create pipe.\n");
+		ExitThread(-1);
+	}
+
+	printf("Server: Waiting for client...\n");
+
+	BOOL connectionResult = ConnectNamedPipe(hPipe, NULL);
+	if (!connectionResult) {
+		printf("Server: Failed to connect to pipe.\n");
+		CloseHandle(hPipe);
+		ExitThread(-1);
+	}
+
+	Stack *stack = (Stack*) HeapAlloc(GetProcessHeap(), 0, sizeof(Stack));
+	*stack = emptyStack();
+
+	char requestBuffer[2];
+	DWORD bytesRead = 0;
+	BOOL readResult = ReadFile(hPipe, requestBuffer, sizeof(char) * 2, &bytesRead, NULL);
+	char stackOpResult = 0;
+	if (readResult) {
+		switch (requestBuffer[0]) {
+		case STACKPICK:
+			printf("Server: Pick -> %d\n", stackPickInt(stack));
+			break;
+		case STACKPOP:
+			printf("Server: Pop -> %d\n", stackPopint(stack));
+			break;
+		case STACKPUSH:
+			stackOpResult = stackPushInt(stack, requestBuffer[1]);
+			if (stackOpResult == -1) {
+				printf("Server: Failed to push.\n");
+			} else {
+				printf("Server: Pushed %d to stack.\n", stackPickInt(stack));
+			}
+			break;
+		case STACKNEW:
+			stackClear(stack);
+			*stack = emptyStack();
+			printf("Server: Created new stack.\n");
+			break;
+		case STACKSIZE:
+			printf("Server: Stack size: %d\n", stack->size);
+			break;
+		case STACKISEMPTY:
+			printf("Server: Stack is %s.", (stack->size > 0 ? "not empty" : "empty"));
+			break;
+		case STACKDISPLAY:
+			printf("Server:\n");
+			displaySatckOfInts(stack);
+			break;
+		}
+	}
+}
+
+void clientJob(LPVOID lpParam) {
+
+}
+
+int main() {
+	Stack *stack = (Stack*) HeapAlloc(GetProcessHeap(), 0, sizeof(Stack));
+	*stack = emptyStack();
+
+	stackPushInt(stack, 124);
+	stackPushInt(stack, 162);
+
+	printf("%d\n", stackPickInt(stack));
+	printf("%d\n", stackPopint(stack));
+	printf("%d\n", stackPopint(stack));
+	printf("%d\n", stackPopint(stack));
+
+	HeapFree(GetProcessHeap(), 0, stack);
 
 	return 0;
 }
