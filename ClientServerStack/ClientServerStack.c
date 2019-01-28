a20c04c05253070bc789115649b0607eaeffdaaf
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-01-28 23:55:16 +0300

    Client and server are being synced

--- ClientServerStack/ClientServerStack.c
+++ ClientServerStack/ClientServerStack.c
@@ -128,137 +128,198 @@ void displaySatckOfInts(Stack *stack) {
 	printf("____________\nSTACK BOTTOM\n\n");
 }
 
-void serverJob(LPVOID lpParam) {
+DWORD WINAPI serverJob(LPVOID lpParam) {
 	HANDLE hPipe = CreateNamedPipe(
 		PIPENAME,
-		PIPE_ACCESS_INBOUND,
+		PIPE_ACCESS_DUPLEX,
 		PIPE_READMODE_BYTE,
 		1, 0, 0, 0, NULL
 	);
 
 	if (hPipe == INVALID_HANDLE_VALUE || hPipe == NULL) {
-		printf("Server: Failed to create pipe.\n");
+		printf("<Server>: Failed to create pipe.\n");
 		ExitThread(-1);
 	}
 
 	BOOL connectionResult = ConnectNamedPipe(hPipe, NULL);
 	if (!connectionResult) {
-		printf("Server: Failed to connect to pipe.\n");
+		printf("<Server>: Failed to connect to pipe.\n");
 		CloseHandle(hPipe);
 		ExitThread(-1);
 	}
 
-	printf("Server: Waiting for client...\n");
+	printf("<Server>: Waiting for client...\n");
+
+	char respond = 0;
+	DWORD bytesRead = 0;
+	BOOL readResult = ReadFile(hPipe, &respond, sizeof(char), &bytesRead, NULL);
+	if (!readResult || respond != 1) {
+		printf("<Server>: Failed to connect to client.\n");
+		CloseHandle(hPipe);
+		ExitThread(-1);
+	}
 
 	Stack *stack = (Stack*) HeapAlloc(GetProcessHeap(), 0, sizeof(Stack));
 	if (!stack) {
-		printf("Server: Failed to allocate memory for stack.\n");
+		printf("<Server>: Failed to allocate memory for stack.\n");
+		CloseHandle(hPipe);
 		ExitThread(-1);
 	}
 	*stack = emptyStack();
 
-	char requestBuffer[2];
-	DWORD bytesRead = 0;
+	int requestBuffer[2];
+	bytesRead = 0;
+	readResult = FALSE;
 	char stackOpResult = 0;
 
+	printf("<Server>: Ready.\n");
+
 	while (TRUE) {
-		BOOL readResult = ReadFile(hPipe, requestBuffer, sizeof(char) * 2, &bytesRead, NULL);
+		readResult = ReadFile(hPipe, requestBuffer, sizeof(int) * 2, &bytesRead, NULL);
 
 		if (readResult) {
 			switch (requestBuffer[0]) {
 			case STACKPICK:
-				printf("Server: Pick -> %d\n", stackPickInt(stack));
+				printf("<Server>: Pick -> %d\n", stackPickInt(stack));
 				break;
 
 			case STACKPOP:
-				printf("Server: Pop -> %d\n", stackPopint(stack));
+				printf("<Server>: Pop -> %d\n", stackPopint(stack));
 				break;
 
 			case STACKPUSH:
 				stackOpResult = stackPushInt(stack, requestBuffer[1]);
 				if (stackOpResult == -1) {
-					printf("Server: Failed to push.\n");
+					printf("<Server>: -> Failed to push.\n");
 				} else {
-					printf("Server: Pushed %d to stack.\n", stackPickInt(stack));
+					printf("<Server>: -> Pushed %d to stack.\n", stackPickInt(stack));
 				}
 				break;
 
 			case STACKNEW:
 				stackClear(stack);
 				*stack = emptyStack();
-				printf("Server: Created new stack.\n");
+				printf("<Server>: -> Created new stack.\n");
 				break;
 
 			case STACKSIZE:
-				printf("Server: Stack size: %llu\n", stack->size);
+				printf("<Server>: Stack size -> %llu\n", stack->size);
 				break;
 
 			case STACKISEMPTY:
-				printf("Server: Stack is %s.", (stack->size > 0 ? "not empty" : "empty"));
+				printf("<Server>: Stack is %s.\n", (stack->size > 0 ? "not empty" : "empty"));
 				break;
 
 			case STACKDISPLAY:
-				printf("Server:\n");
+				printf("<Server>:\n");
 				displaySatckOfInts(stack);
 				break;
 
 			case SERVEREXIT:
+				printf("<Server>: Server stopped.\n");
+				CloseHandle(hPipe);
 				ExitThread(0);
 			}
+
+			respond = 1;
+			DWORD bytesWritten = 0;
+			BOOL writeResult = WriteFile(hPipe, &respond, sizeof(char), &bytesWritten, NULL);
+			if (!writeResult) {
+				printf("<Server>: Failed to send respont to client.\n");
+				CloseHandle(hPipe);
+				ExitThread(-1);
+			}
+
 		} else {
-			printf("Server: Failed to read from pipe.\n");
+			printf("<Server>: Failed to read from pipe.\n");
+			CloseHandle(hPipe);
 			ExitThread(-1);
 		}
 	}
 }
 
 void clientJob(LPVOID lpParam) {
-	HANDLE hPipe = CreateFile(PIPENAME, GENERIC_WRITE,
+	HANDLE hPipe = CreateFile(PIPENAME, GENERIC_READ | GENERIC_WRITE,
 		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
 		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	if (hPipe == INVALID_HANDLE_VALUE || hPipe == NULL) {
-		printf("Client: Failed to connect to pipe.\n");
+		printf("<Client>: Failed to connect to pipe.\n");
 		return;
 	}
 
-	printf("Client: Connected to the server.\n");
-
-	char requestBuffer[2] = {0,0};
+	char respond = 1;
 	DWORD bytesWritten = 0;
+	BOOL writeResult = WriteFile(hPipe, &respond, sizeof(char), &bytesWritten, NULL);
+	if (!writeResult) {
+		printf("<Client>: Failed to connect to the server.\n");
+		CloseHandle(hPipe);
+		return;
+	}
+
+	printf("<Client>: Connected to the server.\n");
+
+	int requestBuffer[2] = {0,0};
 	char inputBuffer[64];
+	respond = 0;
+	writeResult = FALSE;
+	bytesWritten = 0;
+	DWORD bytesRead = 0;
 	while (1) {
 		printf(" > ");
-		scanf_s("%s", inputBuffer, 64);
+		scanf_s("%s", inputBuffer, (unsigned int) sizeof(char) * 64);
 		printf("\n");
 		inputBuffer[63] = '\0';
 
 		if (lstrcmpiA(inputBuffer, "PICK") == 0) {
 			requestBuffer[0] = STACKPICK;
-		} else if (lstrcmpiA(inputBuffer, "POP")) {
+		} else if (lstrcmpiA(inputBuffer, "POP") == 0) {
 			requestBuffer[0] = STACKPOP;
-		} else if (lstrcmpiA(inputBuffer, "PUSH")) {
+		} else if (lstrcmpiA(inputBuffer, "PUSH") == 0) {
 			requestBuffer[0] = STACKPUSH;
-		} else if (lstrcmpiA(inputBuffer, "NEW")) {
+			printf("Value to push? > ");
+			scanf_s("%d", &requestBuffer[1]);
+		} else if (lstrcmpiA(inputBuffer, "NEW") == 0) {
 			requestBuffer[0] = STACKNEW;
-		} else if (lstrcmpiA(inputBuffer, "SIZE")) {
+		} else if (lstrcmpiA(inputBuffer, "SIZE") == 0) {
 			requestBuffer[0] = STACKSIZE;
-		} else if (lstrcmpiA(inputBuffer, "ISEMPTY")) {
+		} else if (lstrcmpiA(inputBuffer, "ISEMPTY") == 0) {
 			requestBuffer[0] = STACKISEMPTY;
-		} else if (lstrcmpiA(inputBuffer, "DISPLAY")) {
+		} else if (lstrcmpiA(inputBuffer, "DISPLAY") == 0) {
 			requestBuffer[0] = STACKDISPLAY;
+		} else if (lstrcmpiA(inputBuffer, "EXIT") == 0) {
+			requestBuffer[0] = SERVEREXIT;
+			CloseHandle(hPipe);
+			return;
+		} else {
+			printf("<Client>: Unknown command.\n");
+			continue;
 		}
 
-		BOOL writeResult = WriteFile(hPipe, requestBuffer, sizeof(char) * 2, &bytesWritten, NULL);
+		writeResult = WriteFile(hPipe, requestBuffer, sizeof(int) * 2, &bytesWritten, NULL);
 		if (!writeResult) {
-			printf("Client: Failed to write to pipe.\n");
+			printf("<Client>: Failed to write to pipe.\n");
+			return;
+		}
+
+		BOOL readResult = ReadFile(hPipe, &respond, sizeof(char), &bytesRead, NULL);
+		if (!readResult) {
+			printf("<Client>: Cannot get respond from server.\n");
+			CloseHandle(hPipe);
+			return;
+		}
+		if (respond != 1) {
+			printf("<Client>: Received wrong respond from server.\n");
+			CloseHandle(hPipe);
 			return;
 		}
 	}
 }
 
 int main() {
-
+	CreateThread(NULL, 0, &serverJob, NULL, 0, NULL);
+	Sleep(1000);
+	clientJob(NULL);
 
 	return 0;
 }
