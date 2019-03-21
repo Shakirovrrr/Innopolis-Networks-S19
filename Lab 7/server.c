8d994c6e3f659e6b3163dbf194e9d7120dddfbe7
Ruslan Shakirov <shakirovrrr@outlook.com>
2019-03-21 18:34:08 +0300

    Clear server code

--- Lab 7/server.c
+++ Lab 7/server.c
@@ -5,68 +5,24 @@
 #include "networking.h"
 #include "linkedlist.h"
 
+#define NODE_SEND_INFO 1
+#define NODE_GET_LIST 2
+#define NODE_GET_PEERS 3
+
 LinkedList *nodepool;
+LinkedList *filepool;
 
 void *node_handler(void *args) {
-	int conn_socket = *((int *) args);
-	int alive = 1;
 
-
-	while (alive) {
-		alive = ping(conn_socket, 1);
-		sleep(1);
-	}
-
-	int port;
-	char ipaddr[INET_ADDRSTRLEN];
-	get_ip_port(conn_socket, &port, (char **) ipaddr);
-	printf("Node %s:%d left.\n", ipaddr, port);
-	close(conn_socket);
-
-	unsigned int ipuint32 = get_uint_ip(conn_socket);
-	deleteVal(nodepool, findVal(nodepool, ipuint32));
 }
 
 void *connections_handler() {
 	int master_socket = init_tcp_server();
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-	while (1) {
-		int sockfd = setup_communication(master_socket);
-		if (sockfd < 0) {
-			perror("Cannot establish TCP connection.\n");
-			continue;
-		}
-
-		int result = 0;
-
-		int readbuf = 0;
-		int sendbuf = 0;
-		result = recvsend(sockfd, (void *) &readbuf, sizeof(int), &sendbuf, sizeof(int));
-		if (result < 0) {
-			perror("Cannot recieve/send message.\n");
-			//exit(EXIT_FAILURE);
-		}
-
-		if (readbuf == 0) {
-			send(sockfd, (const void *) &sendbuf, sizeof(int), 0);
-		}
-
-		if (readbuf == 1) {
-			int port = 0;
-			char ipaddress[INET_ADDRSTRLEN];
-			get_ip_port(sockfd, &port, (char **) ipaddress);
-			printf("Got new node! IP: %s, port: %d", ipaddress, port);
-			pthread_t new_thread;
-			pthread_create(&new_thread, NULL, node_handler, (void *) &sockfd);
-			insertVal(nodepool, 0, get_uint_ip(sockfd));
-		}
-	}
-#pragma clang diagnostic pop
 }
 
 void *node_job(void *args) {
+	struct sockaddr_in *addr = (struct sockaddr_in *) args;
 
 }
 
@@ -75,11 +31,12 @@ int main(int argc, char *argv[]) {
 
 	pthread_t handler_thread = 0;
 	pthread_t node_thread = 0;
+	struct sockaddr_in *addr = NULL;
 	if (argc > 1) {
 		if (strcmp(argv[1], "-main") == 0) {
 			pthread_create(&handler_thread, NULL, connections_handler, NULL);
 		} else {
-			struct sockaddr_in *addr = malloc(sizeof(struct sockaddr_in));
+			addr = malloc(sizeof(struct sockaddr_in));
 			if (convert_address(argv[1], addr) < 0) {
 				perror("Invalid address.\n");
 			}
@@ -87,8 +44,16 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	pthread_join(node_thread, NULL);
-	pthread_join(handler_thread, NULL);
+	if (handler_thread) {
+		pthread_join(handler_thread, NULL);
+	}
+	if (node_thread) {
+		pthread_join(node_thread, NULL);
+	}
+
+	if (addr) {
+		free(addr);
+	}
 
 	return 0;
 }
